93652ba87b11173fa608e618592c8e4455579009
William S. Teixeira <williamsebastiaoteixeira@gmail.com>
2021-10-20 02:37:24 -0300

    Observer

--- controllerConsultaGeral.js
+++ controllerConsultaGeral.js
@@ -1,8 +1,6 @@
-
-//console.log(getQueryVariable("re") + " : " + getQueryVariable("cidade"));
-
-if(getQueryVariable != false){
-    createObserver("row4", a);
+if(getQueryVariable("re") !== false && getQueryVariable("cidade") !== false){
+    console.log(getQueryVariable("re") + " " + getQueryVariable("cidade"));
+    createObserver("row4", a);  
 }
 
 function a(canvas){
@@ -11,9 +9,29 @@ function a(canvas){
 }
 
 function b(canvas){
-    canvas.
-    console.log(canvas);
+
+    canvas.getElementsByTagName('select')[0].value = "ID_EDIFICACAO"; 
+    canvas.getElementsByTagName('input')[0].value = getQueryVariable("re");
+    canvas.getElementsByTagName('select')[1].value = getQueryVariable("cidade");
+
+    /*var $scope = angular.element($0).scope(); */
+
+    /* 
+    var app = angular.module('myApp');
+    app.controller('controllerConsultaGeral', function($scope){
+        $scope.form.objects.tpPesquisa = '{RE : "ID_EDIFICACAO"}'; 
+        //$scope.form.objects.nm_pesquisa_edificacao = '582565832'
+    }); 
+    */
+
+    /*
+    canvas = document.getElementById('row17'); 
+    canvas.getElementsByTagName('button')[1].removeAttribute('disabled');
+    canvas.getElementsByTagName('button')[1].click();
+    createObserver("row17", c); 
+    */
 }
+
 
 function createObserver(element, func){
     var observer = new MutationObserver(function (mutations, me) {
